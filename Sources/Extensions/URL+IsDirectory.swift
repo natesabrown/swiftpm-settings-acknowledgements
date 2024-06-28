e55d72785651f9595f4246bd901f6153d0336374
Nate Brown <nateabrown@icloud.com>
2024-06-27 19:47:26 -0700

    remove unnecessary extension

--- Sources/Extensions/URL+IsDirectory.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension URL {
-  /// Returns `true` if this URL has metadata that confirms it represents the path to a directory.
-  var isDirectory: Bool {
-    (try? self.resourceValues(forKeys: [.isDirectoryKey]).isDirectory) ?? false
-  }
-}
